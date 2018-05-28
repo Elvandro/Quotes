a930fe006124a676b836df62f283553ef11c277e
Elvis Amuni <elvisamuni22@gmail.com>
2018-05-28 12:31:53 +0300

    This commit will implement the pipe that counts the number of days that have passed

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote("Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.","Francis of Assisi","Elvis", 0,0, new Date(2018,5.26)),
-    new Quote("Put your heart, mind, and soul into even your smallest acts. This is the secret of success.","Swami Sivananda","Sanaipei",0,0, new Date(2018,5,25)),
-    new Quote("Happiness is not something you postpone for the future; it is something you design for the present.","Jim Rohn","Lacosta",0,0, new Date(2018,5,24))
+    new Quote("Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.","Francis of Assisi","Elvis",0,0,new Date(2018,5.26)),
+    new Quote("Put your heart, mind, and soul into even your smallest acts. This is the secret of success.","Swami Sivananda","Sanaipei",0,0,new Date(2018,5,25)),
+    new Quote("Happiness is not something you postpone for the future; it is something you design for the present.","Jim Rohn","Lacosta",0,0,new Date(2018,5,24))
   ]
   createNewQuote(quote){
     let quoteLength = this.quotes.length;
