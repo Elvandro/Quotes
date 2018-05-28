5235825283797c57b0763a44b61642603a4275a8
Elvis Amuni <elvisamuni22@gmail.com>
2018-05-28 14:59:14 +0300

    This commit will make modifications to html files

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -7,8 +7,8 @@ export class TimeCountPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     let today:Date = new Date(); // current date and timeCount
-    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var dateDifference = Math.abs(value-todayWithNoTime)//returns value in milliseconds
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate());
+    var dateDifference = Math.abs(value-todayWithNoTime);//returns value in milliseconds
     const secondsInADay = 86400;
 
     var dateDifferenceSeconds = dateDifference*0.001; //converts to seconds
