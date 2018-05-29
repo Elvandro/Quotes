1fe6a94083e35b0c83cacc320f6ca1aea1f96c63
Elvis Amuni <elvisamuni22@gmail.com>
2018-05-29 08:27:59 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    public showDetails:boolean;
-    constructor(public quote:string, public author:string, public person:string, public upVotes:number, public downVotes:number,public dateCreated:Date){
-      this.showDetails=false;
-    }
-}
